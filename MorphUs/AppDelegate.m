93943e34de3c324650338f113c2ec7ebecbdc189
Dan Shepherd <danshepherd@Dans-MacBook-Pro.local>
2015-07-16 02:38:21 +0100

    small bug fix where context was not saving after terminating the application,  for example deleting a project then fully exiting the application and starting it again would bring deleted projects back even after they had been deleted.

--- MorphUs/AppDelegate.m
+++ MorphUs/AppDelegate.m
@@ -53,6 +53,7 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [self saveContext];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -68,6 +69,7 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [self saveContext];
 }
 
 - (void)saveContext
